d9d433eadea60eeb7f6e307a8194d061b2650350
Evgeny Vlasov <vlasov.evgeny@gmail.com>
2022-09-08 01:52:34 +0200

    Улучшил тесты

--- test.sh
+++ test.sh
@@ -4,4 +4,5 @@ mkdir -p ./var/tmp/task-"$1"-tests/ &&
 tr -d '\015' < ./samples/task-"$1"-tests/"$2" > ./var/tmp/task-"$1"-tests/"$2"LF &&
 cat ./var/tmp/task-"$1"-tests/"$2"LF | php ./src/solution-"$1".php > ./var/tmp/task-"$1"-tests/"$2"LF.answer &&
 tr -d '\015' < ./samples/task-"$1"-tests/"$2".a > ./var/tmp/task-"$1"-tests/"$2"LF.reference &&
-diff ./var/tmp/task-"$1"-tests/"$2"LF.answer ./var/tmp/task-"$1"-tests/"$2"LF.reference
+diff ./var/tmp/task-"$1"-tests/"$2"LF.answer ./var/tmp/task-"$1"-tests/"$2"LF.reference &&
+echo 'Test completed successfully!'
