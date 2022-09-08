5ced671b6721b542ba83512855f613f9ef0d2b95
Evgeny Vlasov <vlasov.evgeny@gmail.com>
2022-09-08 13:18:03 +0200

    Уточнение

--- test.sh
+++ test.sh
@@ -5,4 +5,4 @@ tr -d '\015' < ./samples/task-"$1"-tests/"$2" > ./var/tmp/task-"$1"-tests/"$2"LF
 cat ./var/tmp/task-"$1"-tests/"$2"LF | php ./src/solution-"$1".php > ./var/tmp/task-"$1"-tests/"$2"LF.answer &&
 tr -d '\015' < ./samples/task-"$1"-tests/"$2".a > ./var/tmp/task-"$1"-tests/"$2"LF.reference &&
 diff ./var/tmp/task-"$1"-tests/"$2"LF.answer ./var/tmp/task-"$1"-tests/"$2"LF.reference &&
-echo 'Test completed successfully!'
+echo "Test ${1}-${2} completed successfully!"
