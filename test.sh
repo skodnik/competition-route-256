6dc27edd6229edfda04bc4d401ce7d9f6632f4d2
Evgeny Vlasov <vlasov.evgeny@gmail.com>
2022-09-08 23:38:01 +0200

    Причесал, оптимизировал

--- test.sh
+++ test.sh
@@ -8,34 +8,48 @@ TMP_DIR="./var/tmp"
 SMP_DIR="./samples"
 SRC_DIR="./src"
 
-function execute() {
+((PASSED = 0))
+((FAILED = 0))
 
-    mkdir -p ${TMP_DIR}/task-"$1"-tests/ &&
-        cat ${SMP_DIR}/task-"$1"-tests/"$2" | php ${SRC_DIR}/solution-"$1".php >${TMP_DIR}/task-"$1"-tests/"$2".answer &&
-        tr -d '\015' <${SMP_DIR}/task-"$1"-tests/"$2".a >${TMP_DIR}/task-"$1"-tests/"$2".reference &&
-        diff ${TMP_DIR}/task-"$1"-tests/"$2".answer ${TMP_DIR}/task-"$1"-tests/"$2".reference &&
-        printf "%b" "${GREEN}${1}-${2} passed${RESET}\n" || printf "%b" "\n${RED}${1}-${2} failed${RESET}\n"
+function execute() {
+    REFERENCE="${TMP_DIR}/task-${1}/${2}.reference"
+    ANSWER="${TMP_DIR}/task-${1}/${2}.answer"
+
+    mkdir -p "${TMP_DIR}/task-${1}/"
+    php "${SRC_DIR}/solution-${1}.php" <"${SMP_DIR}/task-${1}/${2}" >"${ANSWER}"
+    tr -d '\015' <"${SMP_DIR}/task-${1}/${2}.a" >"${REFERENCE}"
+
+    if diff -c "${ANSWER}" "${REFERENCE}" >"${TMP_DIR}/task-${1}/${2}.diff"; then
+        printf "%b" "${GREEN}${1}-${2} passed${RESET}\n"
+        ((PASSED++))
+    else
+        printf "%b" "${RED}${1}-${2} failed${RESET}\n"
+        ((FAILED++))
+    fi
 }
 
-if [ -z "$1" ]; then
+if [ -z "${1}" ]; then
     printf "%b" "\n${RED}Missing parameter task index${RESET}\n"
     exit 1
 fi
 
-if [ -z "$2" ]; then
-    for f in ${SMP_DIR}/task-"$1"-tests/*; do
+if [ -z "${2}" ]; then
+    for f in "${SMP_DIR}"/task-"${1}"/*; do
         filename="${f##.*/}"
 
-        if [[ $filename == *.a ]]; then
-            continue
+        if ! [[ "${filename}" == *.a ]]; then
+            printf "%b" "\n----------------\n\n"
+            time execute "${1}" "${filename}"
         fi
-
-        printf "%b" "\n----------------\n\n"
-        time execute "$1" "$filename"
     done
+
+    printf "%b" "\n----------------\n"
+    printf "%b" "\nTotal:  $((PASSED + FAILED))"
+    printf "%b" "\n${GREEN}Passed: ${PASSED}${RESET}"
+    printf "%b" "\n${RED}Failed: ${FAILED}${RESET}\n"
 
     exit 0
 fi
 
 printf "%b" "\n"
-time execute "$1" "$2"
+time execute "${1}" "${2}"
